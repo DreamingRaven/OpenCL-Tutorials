6de8fbd927f64c30879eee88837fb10d32881ef0
DreamingRaven <GeorgeRavenNR@outlook.com>
2020-02-24 23:49:36 +0000

    Working tutorial 4 implementation
    Now to get boost into the docker

--- tutorial_4/tut4.cpp
+++ tutorial_4/tut4.cpp
@@ -1,4 +1,14 @@
-#include "Utils.h"
+/**
+ * @Author: GeorgeRaven <archer>
+ * @Date:   2020-02-24T23:43:45+00:00
+ * @Last modified by:   archer
+ * @Last modified time: 2020-02-24T23:46:58+00:00
+ * @License: please see LICENSE file in project root
+ */
+
+
+
+#include "utils.h"
 #include <iostream>
 #include <vector>
 #include <algorithm>
